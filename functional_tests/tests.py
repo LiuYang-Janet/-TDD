659bb1b7e98d3fa160909da8d67b15a436a97c56
liuyang <1620268611@qq.com>
2022-06-04 00:40:28 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,15 +4,16 @@ Created on Tue Mar 29 08:37:13 2022
 
 @author: LY
 """
-from pickle import TRUE
-from stat import SF_APPEND
+
+from tkinter.tix import InputOnly
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 import time
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT=10
+MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -22,20 +23,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
-        start_time=time.time()
+        start_time = time.time()
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
-            except (AssertionError,WebDriverException) as e:
-                if time.time()-start_time>MAX_WAIT:
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
 
-
-        
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app.She goes
         # to check out its homepage
@@ -73,7 +72,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Satisfied, She goes back to sleep
-    
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -84,37 +82,62 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # She notices that her list has a unique URL
-        edith_list_url=self.browser.current_url
-        self.assertRegex(edith_list_url,'/lists/.+')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         # Now a new user, Francis, comes along to the sites.
-        
+
         ## We use a new browser session to make sure taht no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser=webdriver.Firefox()
+        self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
-        page_text=self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers',page_text)
-        self.assertNotIn('make a fly',page_text)
-        
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
-        francis_list_url=self.browser.current_url
-        self.assertRegex(francis_list_url,'/lists/.+')
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
 
         # Again, there is no trace of Edith's list
-        page_text=self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers',page_text)
-        self.assertIn('Buy milk',page_text)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+
+    def test_layout_and_styling(self):
+        #Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #She notices the input box is nicely centered
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,
+            512,
+            delta=10
+        )
+
+        #She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,
+            512,
+            delta=10
+        )
